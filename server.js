e230e41e1758933d94d0211e3e197012beed2b55
Adiretto <a4912520@gmail.com>
2022-12-20 19:41:05 +0600

    dynamic api created

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const products = require("./data");
+const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.get("*", function (req, res) {
+app.use(bodyParser.json());
+app.get("/", function (req, res) {
   res.send("Hello World");
+});
+app.get("/api/products", function (req, res) {
+  const { title } = req.query;
+  const sortedProducts = products.filter((product) =>
+    product.name.includes(title.toLowerCase())
+  );
+  if (sortedProducts.length > 0) {
+    return res.json({ success: true, data: sortedProducts });
+  }
+  res.json({ success: false });
+});
+
+app.get("/images/:imageName", (req, res) => {
+  const { imageName } = req.params;
+  const url = path.join(__dirname, `/images/${imageName}.jpg`);
+  res.sendFile(url);
 });
 
 app.listen(3454, () => {
